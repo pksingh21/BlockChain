d7b09089bebb6db702b4e0c623170a44e8a5b44d
Kaushik Dey <Kaushikdey647@gmail.com>
2022-02-13 15:49:24 +0530

    commit suicide

--- frontEnd/src/App.js
+++ frontEnd/src/App.js
@@ -3,16 +3,33 @@ import { ethers } from "ethers";
 import ErrorMessage from "./ErrorMessage";
 import TxList from "./TxList";
 import AnotherApp from "./Another";
+
+const mainToken = "0xc3379a1bFb1b8B959e7297bD343d6713BbA0926d";
+const childToken = "0x0d8f8f8f8f8f8f8f8f8f8f8f8f8f8f8f8f8f8f8f8";
+
 const startPayment = async ({ setError, setTxs, ether, addr }) => {
   try {
     if (!window.ethereum)
-      throw new Error("No crypto wallet found. Please install it.");
+      throw new Error("Metamask extension not found. Please install it.");
 
     await window.ethereum.send("eth_requestAccounts");
     const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const { chainId } = await provider.getNetwork(); //get chain id
+    let tokenAddr = mainToken;
+    if(chainId == 4){
+      tokenAddr = mainToken;
+    }
+    else if(chainId == 43113){
+      tokenAddr = childToken;
+    }
+    else{
+      throw new Error("You don't seem to be in the correct network, please choose a network from below");
+    }
     const signer = provider.getSigner();
     ethers.utils.getAddress(addr);
+    const gasPrice = provider.getGasPrice();
     const tx = await signer.sendTransaction({
+      contract_address: tokenAddr,
       to: addr,
       value: ethers.utils.parseEther(ether),
     });
@@ -72,7 +89,7 @@ export default function App() {
               type="submit"
               className="btn btn-primary submit-button focus:ring focus:outline-none w-full"
             >
-              Pay now
+              Bridge
             </button>
             <ErrorMessage message={error} />
             <TxList txs={txs} />
